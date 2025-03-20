c113e7737a29330ab849558a2ceab6c4039d920e
Nikita Albert <nikitaalbert@gmail.com>
2025-03-20 12:22:53 -0500

    Updated code to output failing return message, along with changes to accept input test cases

--- findTheString.py
+++ findTheString.py
@@ -15,18 +15,20 @@ import sys
 
 
 def findTheString(lcp):
-    # Write your code here
-   
-    # here we are getting the length of the input string
+    # Here we are getting the length of the input string
     n_length = len(lcp)
    
-    # forming a string array, by the length of input
+    # Forming a string array, by the length of input
     string_array = [""] * n_length
    
-    # setting index for loop
+    # Setting index for loop
     index = 0
-    # assuming only checking for lowercase characters
+    # Assuming only checking for lowercase characters
     lowercase_characters = "abcdefghijklmnopqrstuvwxyz"
+
+    # when this question was posed, it was asked to output "Impossible" on failing conditions
+    # I am storing this message under the variable return_message
+    return_message = "Impossible"
    
     for char in lowercase_characters:
         while index < n_length and string_array[index]:
@@ -38,42 +40,29 @@ def findTheString(lcp):
                 string_array[j_index] = char    
    
     if "" in string_array:
-        return ""
+        return return_message
    
-    for i in range(n_length - 1, -1, -1):
-        for j in range(n_length - 1, -1, -1):
-            if string_array[i] == string_array[j]:
-                if i == n_length - 1 or j == n_length - 1:
-                    if lcp[i][j] != 1:
-                        return ""
-                elif lcp[i][j] != lcp[i + 1][j + 1] + 1:
-                    return ""
-            elif lcp[i][j]:
-                return ""
+    for i_index in range(n_length - 1, -1, -1):
+        for j_index in range(n_length - 1, -1, -1):
+            if string_array[i_index] == string_array[j_index]:
+                if i_index == n_length - 1 or j_index == n_length - 1:
+                    if lcp[i_index][j_index] != 1:
+                        return return_message
+                elif lcp[i_index][j_index] != lcp[i_index + 1][j_index + 1] + 1:
+                    return return_message
+            elif lcp[i_index][j_index]:
+                return return_message
     if "".join(string_array):
         return "".join(string_array)
     else:
-        return "Impossible"                
-   
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+        return return_message               
 
+lcp_test_cases = [
+    [[4,0,2,0],[0,3,0,1],[2,0,2,0],[0,1,0,1]],
+    [[4,3,2,1],[3,3,2,1],[2,2,2,1],[1,1,1,1]],
+    [[8,5,4,8],[7,7,9,1],[4,5,8,1],[9,9,1,7]],
+]
 
-    lcp_rows = int(input().strip())
-    lcp_columns = int(input().strip())
-
-
-    lcp = []
-
-
-    for _ in range(lcp_rows):
-        lcp.append(list(map(int, input().rstrip().split())))
-
-
-    result = findTheString(lcp)
-
-
-    fptr.write(result + '\n')
-
-
-    fptr.close()
+for test_case in lcp_test_cases:
+    result = findTheString(test_case)
+    print("For this test case: ", test_case, "\nThe result is: ", result)
